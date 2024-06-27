7a66f018a341daed7dfc5637a7bd372e6974e8b2
georgeno1ukno <201020588@ingenieria.usac.edu.gt>
2024-06-27 15:50:35 -0600

    Update OpenAI API key configuration in openaiService.ts based on version openai

--- rfqBackend/src/services/openaiService.ts
+++ rfqBackend/src/services/openaiService.ts
@@ -1,14 +1,13 @@
-const { OpenAIApi, Configuration } = require("openai");
+require("dotenv").config();
+import OpenAI from "openai";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY, // defaults to process.env["OPENAI_API_KEY"]
 });
-
-const openai = new OpenAIApi(configuration);
 
 //extracted RFQ data from email content
 export const extractRFQData = async (emailContent: string) => {
-  const response = await openai.createChatCompletion({
+  const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo-0125",
     messages: [
       {
@@ -23,6 +22,7 @@ export const extractRFQData = async (emailContent: string) => {
     ],
   });
 
-  const rfqData = response.data.choices[0].message?.content;
-  return JSON.parse(rfqData);
+  const rfqData = response.choices[0].message?.content;
+  const parsedData = rfqData ? JSON.parse(rfqData) : null;
+  return parsedData;
 };
